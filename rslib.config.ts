4e7a0904a19e6f7785af9040cc3c0f18d45f2757
jsonq <j996730508@163.com>
2025-10-13 14:14:55 +0800

    chore: browser compatibility

--- rslib.config.ts
+++ rslib.config.ts
@@ -1,7 +1,26 @@
 import { pluginReact } from '@rsbuild/plugin-react';
-import { defineConfig } from '@rslib/core';
+import { defineConfig, type LibConfig } from '@rslib/core';
+
+const commonUmdConfig: LibConfig = {
+  bundle: true,
+  format: 'umd',
+  umdName: 'LitePosition',
+  source: {
+    entry: {
+      index: './src/index.ts',
+    },
+  },
+  output: {
+    minify: true,
+    distPath: { root: './dist/browser-polyfill' },
+  },
+};
 
 export default defineConfig({
+  plugins: [pluginReact()],
+  output: {
+    target: 'web',
+  },
   source: {
     tsconfigPath: './tsconfig.build.json',
   },
@@ -9,6 +28,7 @@ export default defineConfig({
     {
       bundle: false,
       dts: true,
+      externalHelpers: true,
       format: 'esm',
       source: {
         entry: {
@@ -20,22 +40,20 @@ export default defineConfig({
       },
     },
     {
-      bundle: true,
-      format: 'umd',
-      umdName: 'LitePosition',
-      source: {
-        entry: {
-          index: './src/index.ts',
-        },
-      },
+      ...commonUmdConfig,
+      syntax: 'es5',
       output: {
-        minify: true,
+        ...commonUmdConfig.output,
+        distPath: { root: './dist/browser-polyfill' },
+      },
+    },
+    {
+      ...commonUmdConfig,
+      externalHelpers: true,
+      output: {
+        ...commonUmdConfig.output,
         distPath: { root: './dist/browser' },
       },
     },
   ],
-  output: {
-    target: 'web',
-  },
-  plugins: [pluginReact()],
 });
