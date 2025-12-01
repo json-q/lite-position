1cadcf2d38544af5557b4b02a47d5adcf6e4326d
jsonq <j996730508@163.com>
2025-12-01 16:19:24 +0800

    fix: getBoundingClientRect not found in test env

--- src/utils/getBoundingClientRect.ts
+++ src/utils/getBoundingClientRect.ts
@@ -1,7 +1,10 @@
+import { EMPTYRect } from 'lite-position/constants';
 import type { ClientRectObject } from '../type';
 
 // source code & update from https://github.com/floating-ui/floating-ui/blob/v2.x/src/dom-utils/getBoundingClientRect.js
 export default function getBoundingClientRect(element: Element): ClientRectObject {
+  if (!element.getBoundingClientRect) return { ...EMPTYRect };
+
   const clientRect = element.getBoundingClientRect();
   let scaleX = 1;
   let scaleY = 1;
